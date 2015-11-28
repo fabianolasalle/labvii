4edbb7f61786643e1e9627b892ba96cc3e81a697
Fabiano <fabiano.fa04@gmail.com>
2015-11-28 17:19:15 -0200

    Inclusão de arquivo de teste Inclusão do arquivo padrão de leitura para a construção do grafo.

---
+++ src/com/ed2/lab7/tests/LabirintoTest.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ed2.lab7.tests;
+
+import org.junit.Test;
+
+import com.ed2.lab7.Labirinto;
+
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Fabiano
+ */
+public class LabirintoTest {
+    Labirinto l = new Labirinto(15, true);
+
+    @Test
+    public void testSize() {
+        assertEquals(l.V(), 15);
+    }
+    
+}
